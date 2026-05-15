2a2bbf894bd3e86952a6a450ba95305fd4299a8a
UpgradedThanYesterday <bikramjit.rajbongshi.official@gmail.com>
2026-05-16 00:23:10 +0530

    Deadlock example

--- src/com/upgradadedthanyesterday/java/concurrency/deadlock/DeadlockDemo.java
+++ src/com/upgradadedthanyesterday/java/concurrency/deadlock/DeadlockDemo.java
@@ -6,16 +6,17 @@ public class DeadlockDemo {
     public static void main(String[] args) {
 
         Thread t1 = new Thread(()->{
-            System.out.println("I am "+Thread.currentThread().getName()+" going inside synchronized block");
             synchronized (LOCK_1) {
                 System.out.println("I am "+Thread.currentThread().getName()+" holding the LOCK_1");
                 try{
                     System.out.println(Thread.currentThread().getName()+" is sleeping for 100 ms");
-                    Thread.sleep(100); // holding LOCK_1 and sleeping for 100ms so by that time, LOCK_2 will be acquired by t2
+                    Thread.sleep(1); // holding LOCK_1 and sleeping for 100ms so by that time, LOCK_2 will be acquired by t2
+                    System.out.println(Thread.currentThread().getName()+" woke up now");
                 } catch (InterruptedException e) {
                     System.out.println("Exception "+e);
                 }
 
+                System.out.println(Thread.currentThread().getName()+" currently here to get the LOCK_2");
                 synchronized(LOCK_2) {
                     System.out.println("I am "+Thread.currentThread().getName()+" holding the LOCK_2");
                 }
@@ -23,15 +24,17 @@ public class DeadlockDemo {
         });
 
         Thread t2 = new Thread(()->{
-            System.out.println("I am "+Thread.currentThread().getName()+" getting inside synchronized block");
             synchronized(LOCK_2) {
                 System.out.println("I am "+Thread.currentThread().getName()+" holding the LOCK_2");
                 try {
                     System.out.println(Thread.currentThread().getName()+" is sleeping for 100 ms");
                     Thread.sleep(100); // holding the LOCK_2 and sleeping for 100ms so by that time, LOCK_1 will be acquired by t1
+                    System.out.println(Thread.currentThread().getName()+" woke up now");
                 } catch (InterruptedException e) {
                     System.out.println("Exception " + e);
                 }
+
+                System.out.println(Thread.currentThread().getName()+" currently here to get the LOCK_1");
                 synchronized(LOCK_1) {
                     System.out.println("I am "+Thread.currentThread().getName()+" holding the LOCK_1");
                 }
