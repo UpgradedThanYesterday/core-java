44cc3c7e22738f6794a667714f2634de87eb3d5a
UpgradedThanYesterday <bikramjit.rajbongshi.official@gmail.com>
2026-05-20 01:26:06 +0530

    DeadlockFixLockOrderExample

--- src/com/upgradadedthanyesterday/java/concurrency/deadlock/DeadlockFixLockOrderExample.java
+++ src/com/upgradadedthanyesterday/java/concurrency/deadlock/DeadlockFixLockOrderExample.java
@@ -1,6 +1,6 @@
 package com.upgradadedthanyesterday.java.concurrency.deadlock;
 
-public class DeadlockFixLokOrderExample {
+public class DeadlockFixLockOrderExample {
     private static final Object LOCK_1 = new Object();
     private static final Object LOCK_2 = new Object();
     public static void main(String[] args) {
